89941ad9dc63a99e798afaacce3615ca57fca449
mahabuburrifat <mahabubur06528@gmail.com>
2025-11-10 19:34:34 +0600

    added hamburger js

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
+const hamburgerIcon = document.querySelector('.hamburger-menu-container');
+const closeIcon = document.querySelector('.close-icon');
+const btnText = document.querySelector('.btn-text');
+const nav = document.querySelector('nav')
+
+
+
+// hamburger open event
+hamburgerIcon.addEventListener('click', (e) => {
+    e.stopPropagation();
+    hamburgerIcon.parentElement.classList.add('menu-open');
+})
+
+
+// hamburger close event
+closeIcon.addEventListener('click', () => {
+    hamburgerIcon.parentElement.classList.remove('menu-open');
+})
+
+
+// window click close hamburger event
+window.addEventListener('click', () => {
+    hamburgerIcon.parentElement.classList.remove('menu-open');
+})
+
+
+// without nav click and close hamburger
+nav.addEventListener('click', (e) => {
+    e.stopPropagation();
+})
 
